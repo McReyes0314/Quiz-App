245a301e72851e2e02c6d75cef842d96540e20cb
Joefhanie <joefhanie@gmail.com>
2024-12-01 06:51:17 +0800

    Updated JS for Answer Sheet

--- js/answerSheetScript.js
+++ js/answerSheetScript.js
@@ -151,21 +151,21 @@ function handleQuestion() {
     for (let i = 0; i <= quizLength; i++) {
         answerBoxContainer.innerHTML += 
         `<div id="answerBox">
-            <h4>${questions[i].question}</h4>
-            <h6>Correct Answer: ${questions[i].correctAnswer}</h6>
-            <p id="placeholder">You answered: ${questions[i].userAnswer}</p>
+            <h4 id="question">${questions[i].question}</h4>
+            <h6 id="correctAnswer">Correct Answer: ${questions[i].correctAnswer}</h6>
+            <h6 id="placeholder">You answered: ${questions[i].userAnswer}</h6>
         </div>`;
 
         let ansBox = document.querySelectorAll('#answerBox');
-        let h4s = document.querySelectorAll('h4');
-        let h6s = document.querySelectorAll('h6');
-        let ps = document.querySelectorAll('p');
+        let quizQuestions = document.querySelectorAll('#question');
+        let correctAnswer = document.querySelectorAll('#correctAnswer');
+        let userAnswer = document.querySelectorAll('#placeholder');
 
         for (let k = 0; k <= i; k++) {
             ansBox[k].classList.add("custom-answer-box");
-            h4s[k].classList.add("questions");
-            h6s[k].classList.add("correct-answer");
-            ps[k].classList.add("placeholder-yourAnswer");
+            quizQuestions[k].classList.add("questions");
+            correctAnswer[k].classList.add("correct-answer");
+            userAnswer[k].classList.add("placeholder-yourAnswer");
         }
     }
     
